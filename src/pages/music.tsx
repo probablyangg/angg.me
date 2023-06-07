f0eb2bd1c42cbef865cf9e7a1d16eb4d628eac57
Angela Gilhotra <angelagilhotra@gmail.com>
2023-06-07 10:53:35 -0300

    soundcloud keeps asking for money, i dont have any :(

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -2,8 +2,12 @@ import Main from "../layout/Main"
 
 const Music = () => {
   return (
-    <Main title="unwind a bit 🕊️">
-      <iframe scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1148686567&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" className="mt-5 h-[550px]"></iframe>
+    <Main>
+      {/* <iframe scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.
+      soundcloud.com/playlists/1148686567&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" className="mt-5 h-[550px]"></iframe> */}
+      <div className="mt-12">
+        <iframe style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/playlist/7BikNFH4eD0ZFNfPS7oISd?utm_source=generator&theme=0" className="h-[550px]" frameBorder="0" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> 
+      </div>
     </Main>
   )
 }
