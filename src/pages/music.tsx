4b511cf1309108313eed08f84e7a0c43720e15fa
Angela Gilhotra <angelagilhotra@gmail.com>
2023-06-29 14:17:43 -0700

    fix player

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -3,10 +3,8 @@ import Main from "../layout/Main"
 const Music = () => {
   return (
     <Main>
-      {/* <iframe scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.
-      soundcloud.com/playlists/1148686567&color=%23000000&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true" className="mt-5 h-[550px]"></iframe> */ }
       <div className="mt-12">
-        <iframe style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/playlist/0AijRQz41WO7DzX1nPnkDs?utm_source=generator" width="100%" height="352" frameBorder="0" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe style={{ borderRadius: "12px", height: "552px" }} src="https://open.spotify.com/embed/playlist/0AijRQz41WO7DzX1nPnkDs?utm_source=generator" width="100%" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
       </div>
     </Main>
   )
