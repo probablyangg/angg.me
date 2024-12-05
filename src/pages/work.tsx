b8cb1573bbb02809513ad890586d32dcec5e1e4c
Angg <angelagilhotra@gmail.com>
2024-12-04 19:32:43 -0800

    Update work.tsx

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -5,10 +5,10 @@ const About = () => {
     <Main title="work stuff">
       <div className="prose text-white my-6 prose-a:text-slate-300 prose-a:underline-offset-8">
         <p>
-          I currently work as a Developer Relations Engineer with <a href="https://layerzero.network/" target="_blank">LayerZero Labs</a> (March 2024 - <i>present</i>).
+          I currently work as a Developer Relations Lead (it's a team of 1 🕺🕺🕺) with Xion (Burnt) since August 2024.
           <br />
           <br />
-          Prior to this, I freelanced as a Developer Advocate with <a href="https://futureprimitive.xyz/" target="_blank">Future Primitive</a> and <a href="https://www.alchemy.com/" target="_blank">Alchemy</a> roughly between June 2023 and January 2024.
+          Prior to this, I freelanced as a Developer Relations Engineer at <a href="https://layerzero.network/" target="_blank">LayerZero Labs</a> (March 2024 - August 2024), Developer Advocate with <a href="https://futureprimitive.xyz/" target="_blank">Future Primitive</a> and <a href="https://www.alchemy.com/" target="_blank">Alchemy</a> roughly between June 2023 and January 2024.
           Previously, I have worked as a Software Engineer at <a href="https://bonfire.xyz" target="_blank">Bonfire</a> (Aug 2021 - April 2023), Software Engineer (Special Projects) at <a href="https://gitcoin.co" target="_blank">Gitcoin</a> (Oct 2020 - Sep 2021) and Blockchain Platform Engineer and a Dev Advocate at <a href="https://polygon.technology" target="_blank">Polygon</a> (Aug 2019 - Oct 2020).
           <br />
           <br />
